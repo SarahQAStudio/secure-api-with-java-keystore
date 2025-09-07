ff320aea1d6126592ff6384cff4c212befe6b3f7
Saranya R C <rcsaranya16@gmail.com>
2025-09-07 16:33:23 +0530

    initial commit

---
+++ run.bat
@@ -0,0 +1,9 @@
+@echo off
+echo ======================================
+echo Starting Secure API with Java Keystore
+echo ======================================
+
+:: Run the Spring Boot JAR
+java -jar secure-api-with-java-keystore.jar
+
+pause
